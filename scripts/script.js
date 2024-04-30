4e36b2a652213b6687e6f908fa5fde31df05b24f
Shayas R.A <shayas.contact@gmail.com>
2024-04-30 21:58:21 +0530

    16 X 16 Grid Created with Javascript and html and styled with css box design

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-console.log('Etch-a-Sketch')
+console.log('Etch-a-Sketch');
+
+let container = document.querySelector('.container');
+containerWidth = container.clientWidth;
+containerHeight = container.clientHeight;
+
+for (let i = 0; i < (16 * 16); i++) {
+    let gridCell = document.createElement('div');
+    gridCell.classList.add('grid-cell')
+    gridCell.style.width = `${containerWidth / 16}px`;
+    gridCell.style.height = `${containerWidth / 16}px`;
+    container.appendChild(gridCell);
+}

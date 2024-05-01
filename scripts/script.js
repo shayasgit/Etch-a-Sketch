761fda9ba005ef7426269b3ec770c15fe41833e9
Shayas R.A <shayas.contact@gmail.com>
2024-05-01 13:15:30 +0530

    Prompted for Grid Size and New Grid Canvas will presented to draw by filling background Grid Size limited within 1 and 100

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,33 @@ let container = document.querySelector('.container');
 containerWidth = container.clientWidth;
 containerHeight = container.clientHeight;
 
-for (let i = 0; i < (16 * 16); i++) {
-    let gridCell = document.createElement('div');
-    gridCell.classList.add('grid-cell')
-    gridCell.style.width = `${containerWidth / 16}px`;
-    gridCell.style.height = `${containerWidth / 16}px`;
-    container.appendChild(gridCell);
+function canvas(noOfGrid, color) {
+    for (let i = 0; i < (noOfGrid * noOfGrid); i++) {
+        let gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell')
+        gridCell.style.width = `${containerWidth / noOfGrid}px`;
+        gridCell.style.height = `${containerWidth / noOfGrid}px`;
+        container.appendChild(gridCell);
+    }
+    
+    let gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach(gridCell => {
+        gridCell.addEventListener('mouseover', () => {
+            gridCell.style.backgroundColor = color;
+        })
+    });
 }
 
-let gridCells = document.querySelectorAll('.grid-cell');
-gridCells.forEach(gridCell => {
-    gridCell.addEventListener('mouseover', () => {
-        gridCell.style.backgroundColor = 'black';
-    })
+canvas(16, 'black');
+
+let askSize = document.querySelector('.prompt');
+askSize.addEventListener('click', () => {
+    let gridSize = 0;
+    do {
+        gridSize = parseInt(prompt("Enter the size of Gird"));
+    } while (gridSize > 100 || gridSize < 1);
+    while(container.hasChildNodes()) {
+        container.removeChild(container.firstChild)
+    }
+    canvas(gridSize, 'black');
 })
